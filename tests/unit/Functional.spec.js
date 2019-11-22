3e77374ed45fcfe0686b4e98db49382fbb9b1a19
palpich <landauvp@gmail.com>
2019-11-22 17:38:51 +0300

    Delete commit

--- tests/unit/Functional.spec.js
+++ tests/unit/Functional.spec.js
@@ -6,15 +6,11 @@ describe('render functional components', () => {
   it('with render', () => {
     const wrapper = mount(FunctionalRender)
 
-    console.log(wrapper.html())
-
     expect(wrapper.is(FunctionalRender)).toBe(true)
   })
 
   it('with template', () => {
     const wrapper = mount(FunctionalTemplate)
-
-    console.log(wrapper.html())
 
     expect(wrapper.is(FunctionalTemplate)).toBe(true)
   })
